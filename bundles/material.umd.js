5918bcb074539038bd985e4795d6f29434c0827b
trik <marco.marche@gnucoop.com>
2019-09-12 10:50:38 +0000

    master - 04310c0 docs: restore test execution with gulp

--- bundles/material.umd.js
+++ bundles/material.umd.js
@@ -33,7 +33,7 @@
 	 * Current version of Ajf.
 	 * @type {?}
 	 */
-	var VERSION = new core.Version('8.2.2-30aef0c');
+	var VERSION = new core.Version('8.2.2-04310c0');
 
 	exports.VERSION = VERSION;
 
--- esm2015/material.js
+++ esm2015/material.js
@@ -29,7 +29,7 @@ import { Version } from '@angular/core';
  * Current version of Ajf.
  * @type {?}
  */
-const VERSION = new Version('8.2.2-30aef0c');
+const VERSION = new Version('8.2.2-04310c0');
 
 export { VERSION };
 //# sourceMappingURL=material.js.map
